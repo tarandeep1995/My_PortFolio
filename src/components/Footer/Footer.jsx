8a5bc50961018afb7e8af9e453e74d765cb4f709
tarandeep1995 <taran933@gmail.com>
2023-04-14 18:26:21 +0530

    change

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ import img from './Untitled Project.jpg'
 const Footer = () => {
   return (
     <div className="footer" style={{ width: "200%", position: 'absolute',
-    height: 'auto', marginTop:"2px"}}>
+    height: 'auto', marginTop:"1px"}}>
       <img src={img} alt="" />
     </div>
   );
